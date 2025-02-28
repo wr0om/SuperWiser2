9cb472a6a78ebb13188e203649e8439923cc1469
Eyal <tadmoreyal@gmail.com>
2025-02-28 15:59:06 +0200

    improved the feecback loop by incorporating history into the generator's chat

--- agents/rag_agent.py
+++ agents/rag_agent.py
@@ -1,8 +1,8 @@
-from qdrant_client import QdrantClient
+# from qdrant_client import QdrantClient
 from agents.agent import *
-from qdrant_client.models import Distance, VectorParams, PointStruct, Batch
+# from qdrant_client.models import Distance, VectorParams, PointStruct, Batch
 import numpy as np
-from transformers import AutoTokenizer, AutoModel
+# from transformers import AutoTokenizer, AutoModel
 import tqdm
 import os
 import torch
@@ -25,19 +25,19 @@ class RAGAgent(Agent):
             """
         self.qdrant_api_key = os.getenv("QDRANT_API_KEY")
         self.qdrant_endpoint = "https://be736619-417f-4ba8-9088-eafbd3c5cc51.us-east4-0.gcp.cloud.qdrant.io:6333"
-        self.qdrant_client = QdrantClient(api_key=self.qdrant_api_key, url=self.qdrant_endpoint)
+        # self.qdrant_client = QdrantClient(api_key=self.qdrant_api_key, url=self.qdrant_endpoint)
         self.embedding_size = 768
         self.collection_name = "Mitzi"
         print(self.qdrant_client.get_collections())
         # self.load_embedder()
 
-    def load_embedder(self, model_str="answerdotai/ModernBERT-base"):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_str)
-        self.model = AutoModel.from_pretrained(model_str)
-        researcher_data ="Mitzi is a cat that likes to run in the Technion annual"
-        researcher_data_embedding = self.embed_text(researcher_data)
-        self.embedding_size = len(researcher_data_embedding)
-        print(f"Embedding size: {self.embedding_size}")
+    # def load_embedder(self, model_str="answerdotai/ModernBERT-base"):
+    #     self.tokenizer = AutoTokenizer.from_pretrained(model_str)
+    #     self.model = AutoModel.from_pretrained(model_str)
+    #     researcher_data ="Mitzi is a cat that likes to run in the Technion annual"
+    #     researcher_data_embedding = self.embed_text(researcher_data)
+    #     self.embedding_size = len(researcher_data_embedding)
+    #     print(f"Embedding size: {self.embedding_size}")
 
     def embed_text(self, text):
         return self.model(**self.tokenizer(text, return_tensors="pt")).last_hidden_state[0, -1].detach().numpy()
@@ -150,26 +150,26 @@ class RAGAgent(Agent):
         return super().generate_response(formatted_prompt)
 
 
-    def load_data(self, documents, embedding_size=768):
-        # Create the collection if it doesn't exist
-        if not self.qdrant_client.collection_exists(self.collection_name):
-            self.qdrant_client.create_collection(
-                collection_name=self.collection_name,
-                vectors_config=VectorParams(size=embedding_size, distance=Distance.COSINE),  # Adjust size and distance as needed
-            )
+    # def load_data(self, documents, embedding_size=768):
+    #     # Create the collection if it doesn't exist
+    #     if not self.qdrant_client.collection_exists(self.collection_name):
+    #         self.qdrant_client.create_collection(
+    #             collection_name=self.collection_name,
+    #             vectors_config=VectorParams(size=embedding_size, distance=Distance.COSINE),  # Adjust size and distance as needed
+    #         )
 
-        # Upsert points into the collection
-        ids = [doc["id"] for doc in documents]
-        vectors = [doc["vector"] for doc in documents]
-        payloads = [doc["payload"] for doc in documents]
+    #     # Upsert points into the collection
+    #     ids = [doc["id"] for doc in documents]
+    #     vectors = [doc["vector"] for doc in documents]
+    #     payloads = [doc["payload"] for doc in documents]
 
-        self.qdrant_client.upsert(
-            collection_name=self.collection_name,
-            points=Batch(ids=ids, vectors=vectors, payloads=payloads)
-        )
+    #     self.qdrant_client.upsert(
+    #         collection_name=self.collection_name,
+    #         points=Batch(ids=ids, vectors=vectors, payloads=payloads)
+    #     )
 
-        stored_points = self.qdrant_client.scroll(collection_name=self.collection_name, limit=10, with_payload=True, with_vectors=True)
-        print(stored_points)
+    #     stored_points = self.qdrant_client.scroll(collection_name=self.collection_name, limit=10, with_payload=True, with_vectors=True)
+    #     print(stored_points)
 
     def load_documents(self, doc_path="web_scraping/researchers_db"):
         # load data from web_scraping/researchers_db folder
